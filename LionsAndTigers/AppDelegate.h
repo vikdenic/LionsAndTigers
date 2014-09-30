e1ead692174b2f217d923b1cc7dd7722fe0c880c
vikdenic <vik.denic@gmail.com>
2014-09-30 09:32:07 -0500

    Initial Commit

---
+++ LionsAndTigers/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LionsAndTigers
+//
+//  Created by Vik Denic on 9/30/14.
+//  Copyright (c) 2014 MobileMakers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LionsAndTigers/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LionsAndTigers
+//
+//  Created by Vik Denic on 9/30/14.
+//  Copyright (c) 2014 MobileMakers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
